3f0eeb13f8eb032504efaa7192aa948090b87a2e
Agustín Chazarreta <agustinchazarreta00@gmail.com>
2025-01-24 12:29:42 -0300

    Diseño de main-page finalizado

--- js/main.js
+++ js/main.js
@@ -1,29 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // Select the container for displaying countries
     const countriesContainer = document.querySelector("#countries-container");
     const baseAPIUrl = `https://restcountries.com/v3.1/all`;    
     let currentAPIUrl = baseAPIUrl;  
     
+    // Select elements for dark mode toggle and pagination buttons
     const darkModeToggle = document.querySelector('.dark-mode-toggle');
     const body = document.body;
     const prevBtn = document.getElementById("prev-btn");
     const nextBtn = document.getElementById("next-btn");
     const pageInfo = document.getElementById("page-info");
     
+    // Initialize pagination variables
     let currentPage = 1;
     let skip = 0;
     const limit = 8;
     let totalCountries = 0;
     
+    // Initialize region filter
     let selectedRegion = "All countries";
     const filterCountry = document.querySelector(".filter-country");
-    const searchInput = document.querySelector('.search-country'); // Captura el input de búsqueda
-    let allCountries = []; // Almacena todos los países para filtrar
-    let filteredCountries = "";
+    const searchInput = document.querySelector('.search-country'); 
+    let allCountries = []; 
+    let filteredCountries = [];
     
+    // Event listener for region filter change
     filterCountry.addEventListener("change", () => {
         const newRegion = filterCountry.value;
         if (newRegion !== selectedRegion) { 
             selectedRegion = newRegion;
+            // Update API URL based on selected region
             currentAPIUrl = (selectedRegion && selectedRegion !== "All countries")
                 ? `https://restcountries.com/v3.1/region/${selectedRegion.toLowerCase()}`
                 : baseAPIUrl;
@@ -32,57 +38,54 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
+    // Event listener for search input
     searchInput.addEventListener("input", (event) => {
         const searchTerm = event.target.value.toLowerCase();
-        currentPage = 1; // Reinicia la página a 1 al buscar
-    
-        // Filtra los países según el término de búsqueda
+        currentPage = 1; 
+        
+        // Filter countries based on search term
         filteredCountries = allCountries.filter(country => 
             country.name.common.toLowerCase().includes(searchTerm)
         );
-        totalCountries = filteredCountries.length; // Actualiza el total de países filtrados
+        totalCountries = filteredCountries.length; 
         
         skip = (currentPage - 1) * limit;
-        // Actualiza la visualización de países filtrados
         displayCountries(filteredCountries.slice(skip, skip + limit));
-        actualizarInfoPaginacion(); // Actualiza la información de paginación
+        actualizarInfoPaginacion(); 
     });
     
+    // Function to fetch countries from the API
     function fetchCountries(page) {
         skip = (page - 1) * limit;
     
         fetch(currentAPIUrl)
             .then((response) => response.json())
             .then((data) => {
-                allCountries = data; // Almacena todos los países aquí
+                allCountries = data; 
                 totalCountries = data.length; 
                 let countries = "";
 
-                // Si hay un término de búsqueda, filtra los países
+                // Check if search input is not empty
                 if (!isSearchInputEmpty()) {
                     filteredCountries = allCountries.filter(country => 
                         country.name.common.toLowerCase().includes(searchInput.value.toLowerCase())
                     );
-                    totalCountries = filteredCountries.length; // Actualiza el total de países filtrados
+                    totalCountries = filteredCountries.length; 
                     countries = ordenarPorClave(filteredCountries, "name.common");
-                }else {
+                } else {
                     countries = ordenarPorClave(allCountries, "name.common");
                 } 
                 
                 const paginatedData = countries.slice(skip, skip + limit);
-    
-                // Limpia el contenedor de cards
                 countriesContainer.innerHTML = "";
-    
-                // Genera las cards de países
                 displayCountries(paginatedData);
                 actualizarInfoPaginacion();
             })
             .catch((error) => console.error("Error fetching products:", error));
     }
     
+    // Function to display countries in the container
     function displayCountries(countries) {
-        // Limpia el contenedor de cards
         countriesContainer.innerHTML = "";
     
         countries.forEach((country) => {
@@ -99,12 +102,13 @@ document.addEventListener("DOMContentLoaded", () => {
                 </div>  
             </div>
             `;
-    
+
+            // Event listener for country card click
             countryCard.addEventListener("click", () => {
                 if(country.ccn3 !== undefined){
-                    window.location.href = `country.html?ccn3=${encodeURIComponent(country.ccn3)}`;
+                    window.location.href = `country-detail-page.html?ccn3=${encodeURIComponent(country.ccn3)}`;
                 } else {
-                    window.location.href = `country.html?cca3=${encodeURIComponent(country.cca3)}`;
+                    window.location.href = `country-detail-page.html?cca3=${encodeURIComponent(country.cca3)}`;
                 }
             });
     
@@ -112,22 +116,22 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
+    // Function to check if the search input is empty
     function isSearchInputEmpty() {
-        return searchInput.value.trim() === ""; // Verifica si el campo de búsqueda está vacío
+        return searchInput.value.trim() === ""; 
     }
 
-    // Función para actualizar la información de paginación
+    // Function to update pagination information
     function actualizarInfoPaginacion() {
         pageInfo.textContent = `Página ${currentPage}`;
         prevBtn.disabled = currentPage === 1;
         nextBtn.disabled = (currentPage * limit) >= totalCountries;
     }
 
+    // Function to sort an array by a specified key
     function ordenarPorClave(array, clave) {
         return array.sort((a, b) => {
-          // Dividimos la clave anidada en partes (por ejemplo, "name.common" -> ["name", "common"])
         const keys = clave.split(".");
-        // Función para acceder al valor de una clave anidada
         const obtenerValor = (obj, keys) => keys.reduce((acc, key) => acc?.[key], obj);
         const valorA = obtenerValor(a, keys)?.toString().toLowerCase() || "";
         const valorB = obtenerValor(b, keys)?.toString().toLowerCase() || "";
@@ -135,30 +139,29 @@ document.addEventListener("DOMContentLoaded", () => {
     });
     }
 
+    // Function to handle dark mode toggle
     function changeDarkMode(){
-        // Verifica si el modo oscuro está activado en el almacenamiento local
         if (localStorage.getItem('dark-mode') === 'enabled') {
             body.classList.add('dark-mode');
-            darkModeToggle.innerHTML = `<ion-icon class="toggle-icon" name="moon-outline"></ion-icon> Dark Mode`;// Cambia el ícono al cargar
-        }else {darkModeToggle.innerHTML = `<ion-icon class="toggle-icon" name="sunny-outline"></ion-icon> Light Mode`;}
+            darkModeToggle.innerHTML = `<ion-icon class="toggle-icon" name="moon-outline"></ion-icon> Dark Mode`;
+        } else {
+            darkModeToggle.innerHTML = `<ion-icon class="toggle-icon" name="sunny-outline"></ion-icon> Light Mode`;
+        }
         
-        
-        // Cambia el modo al hacer clic en el botón
         darkModeToggle.addEventListener('click', () => {
             body.classList.toggle('dark-mode');
         
-            // Actualiza el ícono según el modo actual
             if (body.classList.contains('dark-mode')) {
-                darkModeToggle.innerHTML = `<ion-icon class="toggle-icon" name="moon"></ion-icon> Dark Mode`; // Cambia el texto a "Dark Mode"
-                localStorage.setItem('dark-mode', 'enabled'); // Guarda la preferencia
+                darkModeToggle.innerHTML = `<ion-icon class="toggle-icon" name="moon"></ion-icon> Dark Mode`; 
+                localStorage.setItem('dark-mode', 'enabled'); 
             } else {
-                darkModeToggle.innerHTML = `<ion-icon class="toggle-icon" name="sunny-outline"></ion-icon> Light Mode`; // Cambia el texto a "Light Mode"
-                localStorage.setItem('dark-mode', 'disabled'); // Guarda la preferencia
+                darkModeToggle.innerHTML = `<ion-icon class="toggle-icon" name="sunny-outline"></ion-icon> Light Mode`; 
+                localStorage.setItem('dark-mode', 'disabled'); 
             }
         });
     }
     
-    // Eventos de los botones de paginación
+    // Event listeners for pagination buttons
     prevBtn.addEventListener("click", () => {
         currentPage--;
         fetchCountries(currentPage);
@@ -169,9 +172,7 @@ document.addEventListener("DOMContentLoaded", () => {
         fetchCountries(currentPage);
     });
 
-
+    // Initial fetch of countries and setup of dark mode
     fetchCountries(currentPage);
     changeDarkMode();
-    });
-    
-
+});
